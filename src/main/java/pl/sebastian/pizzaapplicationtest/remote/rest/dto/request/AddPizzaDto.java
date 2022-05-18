37762aa0d24e9472dfb11238ea6033a3ff0cffdc
sebe324 <sebeq05@gmail.com>
2022-05-18 14:43:06 +0200

    :)

--- src/main/java/pl/sebastian/pizzaapplicationtest/remote/rest/dto/request/AddPizzaDto.java
+++ src/main/java/pl/sebastian/pizzaapplicationtest/remote/rest/dto/request/AddPizzaDto.java
@@ -4,4 +4,37 @@ public class AddPizzaDto {
     private int id;
     private int sizeId;
     private int count;
+
+    public AddPizzaDto() {
+    }
+
+    public AddPizzaDto(int id, int sizeId, int count) {
+        this.id = id;
+        this.sizeId = sizeId;
+        this.count = count;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSizeId() {
+        return sizeId;
+    }
+
+    public void setSizeId(int sizeId) {
+        this.sizeId = sizeId;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
